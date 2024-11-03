df717ad2c875eed6230a3d2a24263c138078e984
bluewolfJ124 <bluewolfJ124@gmail.com>
2024-11-03 18:49:52 +1000

    changed css

--- main.py
+++ main.py
@@ -205,4 +205,5 @@ def resetpassword():
         return render_template("resetpass.html", name=get_username())
 
 if __name__ == '__main__':
-    app.run(debug=False, host="0.0.0.0")
+    init_sqlite_db()
+    app.run(debug=True, host="0.0.0.0")
